897543159d3ca0dd6dcadc3f7907dc5e44cf37e6
jemand771 <jemand771@gmx.net>
2020-08-27 01:02:25 +0200

    added basic rainbow effect (might expand to universal colorloop effect), should work as-is

--- queuecommand.py
+++ queuecommand.py
@@ -7,5 +7,10 @@ class QueueCommand:
         self.kwargs = kwargs
         pass
 
+    def set_details(self, starttime, device, pixels):
+        self.time += starttime
+        self.kwargs["pixels"] = pixels
+        self.kwargs["device"] = device
+
     def __repr__(self):
         return "<QueueCommand time " + str(self.time) + ">"
