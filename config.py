38de6d59aa7457fb1034ae0f5bc8cfb6b73c3c04
jemand771 <jemand771@gmx.net>
2020-10-12 16:43:40 +0200

    reworked config (moved to config/config.json)

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-#todo move this to config folder/json file?
-tps = 20
-mspt = 1 / tps
-bind_address = "192.168.0.14"
+import json
+
+# NOTE: config values should not be changed during runtime as things might break.
+# just restart the program after making changes to config.json
+with open("config/config.json") as f:
+    js = json.load(f)
+    tps = js.get("tps", 20)
+    mspt = 1 / tps
+    bind_address = js.get("bind_address", "0.0.0.0")
